8005b00f7ea830c334cf73c0e429eae950ef04d4
Fabien Lenoir <flenoir@gmail.com>
2014-12-27 11:59:01 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Alpaga::Application.config.secret_key_base = '1e31375415c4007e9a0c41635ef5b20a43068fa8e365ed6947d4a35bb51b5171b9ac84e5c906a489a6ebb8181cc1efa3656315e69ff5b69bdfe5da890fd6258f'
